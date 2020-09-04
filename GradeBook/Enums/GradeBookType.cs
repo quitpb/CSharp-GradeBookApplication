7a23ffe6b06dd9b6795965e9c85df7f6b80937cc
qpb96 <quitpb@stud.ntnu.no>
2020-09-04 14:05:28 +0200

    Fix prop

--- GradeBook/Enums/GradeBookType.cs
+++ GradeBook/Enums/GradeBookType.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace GradeBook.Enums
 {
-    class GradeBookType
+    public enum GradeBookType
     {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
     }
 }
